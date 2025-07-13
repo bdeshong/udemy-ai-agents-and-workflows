0647a137cfeb467692f0e65f1b4e3218a9ebfe9c
Brian DeShong <brian@deshong.net>
2025-07-13 19:41:44 -0400

    Integrate better prompt and example data.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import json
 from openai import OpenAI
 
 load_dotenv()
@@ -7,25 +8,45 @@ client = OpenAI()
 
 
 def generate_x_post(topic: str) -> str:
+    with open("post-examples.json", "r") as f:
+        examples = json.load(f)
+
+    examples_str = ""
+    for i, example in enumerate(examples, 1):
+        examples_str += f"""
+        <example-{i}>
+            <topic>
+            {example["topic"]}
+            </topic>
+
+            <generated-post>
+            {example["post"]}
+            </generated-post>
+        </example-{i}>
+        """
+
     prompt = f"""
-        You are an expert social media manager, and you excel at crafting viral and highly engaging posts for the X platform (formerly Twitter).
+        You are an expert social media manager, and you excel at crafting viral and highly engaging posts for X (formerly Twitter).
 
         Your task is to generate a post that is concise, impactful, and tailored to the topic provided by the user.
+        Avoid using hashtags and lots of emojis (a few emojis are okay, but not too many).
 
-        Avoid using hashtags and lost of emojis (a few emojis are okay, but not too many).
+        Keep the post short and focused, structure it in a clean, readable way, using line breaks and empty lines to enhance readability.
 
-        Keep the post short and focused, structure it in a clean, readable way using linebreaks and empty lines to enhance readability.
-
-        Here's the topic provided by the user for which to need to generate a post:
+        Here's the topic provided by the user for which you need to generate a post:
         <topic>
-            {topic}
+        {topic}
         </topic>
-    """
 
-    response = client.responses.create(
-        model="gpt-4o",
-        input=prompt,
-    )
+        Here are some examples of topics and generated posts:
+        <examples>
+            {examples_str}
+        </examples>
+
+        Please use the tone, language, structure, and style of the examples provided above to generate a post that is engaging and relevant to the topic provided by the user.
+        Don't use the content from the examples!
+"""
+    response = client.responses.create(model="gpt-4o", input=prompt)
 
     return response.output_text
 
