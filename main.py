75625e04806734f22faf20a57ae507aed2cad8da
Brian DeShong <brian@deshong.net>
2025-07-13 19:14:21 -0400

    Add initial prompt.

--- main.py
+++ main.py
@@ -7,10 +7,25 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
-def generate_x_post(user_input: str) -> str:
+def generate_x_post(topic: str) -> str:
+    prompt = f"""
+        You are an expert social media manager, and you excel at crafting viral and highly engaging posts for the X platform (formerly Twitter).
+
+        Your task is to generate a post that is concise, impactful, and tailored to the topic provided by the user.
+
+        Avoid using hashtags and lost of emojis (a few emojis are okay, but not too many).
+
+        Keep the post short and focused, structure it in a clean, readable way using linebreaks and empty lines to enhance readability.
+
+        Here's the topic provided by the user for which to need to generate a post:
+        <topic>
+            {topic}
+        </topic>
+    """
+
     payload = {
-        "model": "",
-        "input": "",
+        "model": "gpt-4o",
+        "input": prompt,
     }
 
     response = request.post(
